dabb7051517cacedf6478afbb9c645555885c830
kyrieluo <luot918@163.com>
2023-09-08 00:21:48 +0800

    tmp

--- main.go
+++ main.go
@@ -11,58 +11,18 @@ func main() {
 
 	//val := []*ListNode{NewListNodeS(1, 4, 5), NewListNodeS(1, 3, 4), NewListNodeS(2, 6)}
 
-	v := swapPairs(l1)
+	v := reverseKGroup(l1, 3)
 	fmt.Println(v)
 }
 
-//给你一个链表数组，每个链表都已经按升序排列。
+//给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
 //
-//请你将所有链表合并到一个升序链表中，返回合并后的链表。
+//k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
 //
-//示例 1：
-//
-//输入：lists = [[1,4,5],[1,3,4],[2,6]]
-//输出：[1,1,2,3,4,4,5,6]
-//解释：链表数组如下：
-//[
-//1->4->5,
-//1->3->4,
-//2->6
-//]
-//将它们合并到一个有序链表中得到。
-//1->1->2->3->4->4->5->6
-//示例 2：
-//
-//输入：lists = []
-//输出：[]
-//示例 3：
-//
-//输入：lists = [[]]
-//输出：[]
-//
-//
-//提示：
-//
-//k == lists.length
-//0 <= k <= 10^4
-//0 <= lists[i].length <= 500
-//-10^4 <= lists[i][j] <= 10^4
-//lists[i] 按 升序 排列
-//lists[i].length 的总和不超过 10^4
+//你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
 
-func swapPairs(head *ListNode) *ListNode {
-	var cursorNode = &ListNode{}
-	var headNode = cursorNode
-
-	cursorNode.Next = head
-	for cursorNode.Next != nil && cursorNode.Next.Next != nil {
-		first := cursorNode.Next
-		second := cursorNode.Next.Next
-		first.Next = second.Next
-		second.Next = first
-		cursorNode.Next = second
-		cursorNode = cursorNode.Next.Next
-	}
-
-	return headNode.Next
+//输入：head = [1,2,3,4,5], k = 3
+//输出：[3,2,1,4,5]
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	return nil
 }
