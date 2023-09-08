19eb9c539d1d391f149729b24e971b1cf683c194
罗天（Kyrie/莱德） <Luo.Tian@olaola.chat>
2023-09-08 18:02:34 +0800

    add no.25

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 
 func main() {
 	//[[1,4,5],[1,3,4],[2,6]]
-	l1 := NewListNodeS(1, 2, 3, 4, 5)
+	//l1 := NewListNodeS(1, 2, 3, 4, 5)
 	//val := []*ListNode{NewListNodeS(1, 4, 5), NewListNodeS(1, 3, 4), NewListNodeS(2, 6)}
-	v := reverseKGroup(l1, 2)
+	//val := []int{3, 2, 2, 3}
+	v := strStr("aabaaabaaac", "aabaaac")
 	//v := reverseList(l1)
 	fmt.Println(v)
 }
@@ -19,41 +20,31 @@ func main() {
 //k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
 //
 //你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+func strStr(haystack string, needle string) int {
+	for i := 0; i < len(haystack); i++ {
+		if haystack[i] == needle[0] {
+			fIdx := i
+			cur := i
+			var newI = -2
 
-//输入：head = [1,2,3,4,5], k = 3
-//输出：[3,2,1,4,5]
-func reverseKGroup(head *ListNode, k int) *ListNode {
-	dummy := &ListNode{Next: head}
-
-	pre := dummy
-	end := dummy
-	for end.Next != nil {
-		for i := 0; i < k; i++ {
-			end = end.Next
-			if end == nil {
-				return dummy.Next
+			cur++
+			for cur < len(haystack) && cur-fIdx < len(needle) && haystack[cur] == needle[cur-fIdx] {
+				if newI == -2 && haystack[cur] == needle[0] {
+					newI = cur - 1
+				}
+				cur++
 			}
+
+			if cur-fIdx == len(needle) {
+				return fIdx
+			}
+
+			if newI == -2 {
+				newI = cur - 1
+			}
+			i = newI
 		}
-
-		start := pre.Next
-		next := end.Next
-		end.Next = nil
-		pre.Next = reverseList(start)
-		start.Next = next
-		pre = start
-		end = pre
 	}
 
-	return dummy.Next
-}
-
-func reverseList(head *ListNode) *ListNode {
-	var pre *ListNode
-	for head != nil {
-		next := head.Next
-		head.Next = pre
-		pre = head
-		head = next
-	}
-	return pre
+	return -1
 }
